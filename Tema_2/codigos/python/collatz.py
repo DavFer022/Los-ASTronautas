ad0a47546eddb0d5717ea10152880cebcf837566
Sheen Alburquerque <sheen1386@gmail.com>
2026-05-31 13:17:55 -0400

    Create collatz.py
    Enunciado de Collatz en Python

---
+++ Tema_2/codigos/python/collatz.py
@@ -0,0 +1,15 @@
+"""
+ALGORITMO DE COLLATZ - BENCHMARKING
+Tema 2 - Lenguajes y Compiladores
+Grupo: Los ASTronautas
+
+Enunciado:
+Demostrar la conjetura de Collatz para todos los números menores que N.
+Para cada número n, se aplica:
+- si n es par: n = n / 2
+- si n es impar: n = 3 * n + 1
+El proceso termina cuando n = 1.
+
+Este programa mide el tiempo de ejecución para N = 100,000
+y calcula el total de pasos realizados.
+"""
